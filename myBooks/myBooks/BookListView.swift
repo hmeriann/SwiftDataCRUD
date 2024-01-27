2d375982ea901f9cb25ae24bc9fd9c8cf107065a
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-01-27 19:08:16 +0100

    in a BookListView to the Toolbar added functionality to show a sheet with a NewBookView on the half screen and the input data is saved in the DB

--- myBooks/myBooks/BookListView.swift
+++ myBooks/myBooks/BookListView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct BookListView: View {
+    // we need to present a sheet (NewBookView) on tap on + button
+    @State private var createNewBook = false
     var body: some View {
         NavigationStack {
             VStack {
@@ -20,11 +22,16 @@ struct BookListView: View {
             .navigationTitle("My Books")
             .toolbar {
                 Button {
-                    
+                    createNewBook = true
                 } label: {
                     Image(systemName: "plus.circle.fill")
                         .imageScale(.large)
                 }
+            }
+            // after a toolbar adding a sheet presenter depending on a state of the createNewBook property - it will present NewBookView not on the whole screen. And we need to switch createNewBook property to true, when + Button is tapped
+            .sheet(isPresented: $createNewBook) {
+                NewBookView()
+                    .presentationDetents([.medium])
             }
         }
     }
