6e256180319faf1c9a0b80689f98e0a5aad860e9
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-01-27 18:56:51 +0100

    Added a continer to store the information in the DB and called it context, created new book and inserted it into the context

--- myBooks/myBooks/NewBookView.swift
+++ myBooks/myBooks/NewBookView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct NewBookView: View {
+    // swift stores data in a container called Context. To have an access to it we create a container called context and on tap on the Create button will be able to create a new book
+    @Environment(\.modelContext) private var context
     @Environment(\.dismiss) var dismiss
     @State private var title = ""
     @State private var author = ""
@@ -18,6 +20,9 @@ struct NewBookView: View {
                 TextField("Book Title", text: $title)
                 TextField("Author", text: $author)
                 Button("Create") {
+                    // here we can create a new book using state variables and insert it into a context
+                    let newBook = Book(title: title, author: author)
+                    context.insert(newBook)
                     dismiss()
                 }
                 .frame(maxWidth: .infinity, alignment: .trailing)
