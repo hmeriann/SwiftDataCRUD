15b7b4eedfc7c118433a4bfcc6bf66d1927678a9
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-01-28 20:11:56 +0100

    Added a ContentUnavailableView

--- myBooks/myBooks/BookListView.swift
+++ myBooks/myBooks/BookListView.swift
@@ -15,33 +15,41 @@ struct BookListView: View {
     @State private var createNewBook = false
     var body: some View {
         NavigationStack {
-            // replace VStack with the List
-            List {
-                ForEach(books) { book in
-                    NavigationLink {
-                        Text(book.title)
-                    } label: {
-                        HStack(spacing: 10) {
-                            book.icon
-                            VStack(alignment: .leading) {
-                                Text(book.title).font(.title2)
-                                Text(book.author).foregroundStyle(.secondary)
-                                if let rating = book.rating {
-                                    HStack {
-                                        ForEach(0..<rating, id: \.self) { _ in
-                                            Image(systemName: "star.fill")
-                                                .imageScale(.small)
-                                                .foregroundStyle(.yellow)
+            Group {
+                if books.isEmpty {
+                    ContentUnavailableView(
+                        "Enter your first book.",
+                        systemImage: "book.fill"
+                    )
+                } else {
+                    // replace VStack with the List
+                    List {
+                        ForEach(books) { book in
+                            NavigationLink {
+                                Text(book.title)
+                            } label: {
+                                HStack(spacing: 10) {
+                                    book.icon
+                                    VStack(alignment: .leading) {
+                                        Text(book.title).font(.title2)
+                                        Text(book.author).foregroundStyle(.secondary)
+                                        if let rating = book.rating {
+                                            HStack {
+                                                ForEach(0..<rating, id: \.self) { _ in
+                                                    Image(systemName: "star.fill")
+                                                        .imageScale(.small)
+                                                        .foregroundStyle(.yellow)
+                                                }
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                    .listStyle(.plain)
                 }
             }
-            .listStyle(.plain)
-            .padding()
             .navigationTitle("My Books")
             .toolbar {
                 Button {
@@ -57,10 +65,10 @@ struct BookListView: View {
                     .presentationDetents([.medium])
             }
         }
+        }
     }
-}
-
-#Preview {
-    BookListView()
-        .modelContainer(for: Book.self, inMemory: true)
-}
+    
+    #Preview {
+        BookListView()
+            .modelContainer(for: Book.self, inMemory: true)
+    }
